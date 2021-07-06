aabe6911f2ebda940b80eab3725ee154817176d1
Meagan Campbell <meagancampbell@users.noreply.github.com>
2021-07-06 18:28:39 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,34 @@
+
+
+function removeI(arr) {
+    if (arr.indexOf('i') === -1) {
+
+      return arr;
+    } else {
+      arr.splice(arr.indexOf('i'),1);
+      return removeI(arr);
+    }
+};
+
+let arrayToChange = ['One', 'i', 'c', 'X', 'i', 'i', 54];
+
+console.log(removeI(arrayToChange));
+console.log('______');
+// !arr.includes('i')
+// arr.includes('i')
+// arr.indexOf('i')===-1
+// arr.indexOf('i') !== -1
+
+
+function removeI(arr) {
+    if (!arr.includes('i')) {
+
+      return arr;
+    } else {
+      arr.splice(arr.indexOf('i'), 1);
+      return removeI(arr);
+    }
+};
+
+
+console.log(removeI(arrayToChange));
